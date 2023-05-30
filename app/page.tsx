89373210899c510359cdbb4e7913cd72358ab58d
B-HANSEN <b.hansen.ubiqum@gmail.com>
2023-05-30 19:35:39 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className='text-blue-400 text-7xl'>Howdy MF's!!</h1>
+
+    </main>
+  )
+}

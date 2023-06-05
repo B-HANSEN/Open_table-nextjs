6e43ffb76a660a1f6b5abac4645d72f0801e0da4
B-HANSEN <b.hansen.ubiqum@gmail.com>
2023-06-05 17:08:16 +0200

    created postgresql database in supabse, set up env variable and installed prisma package to communicate with database, setup db calls, dynamically render info from db and routes, created schemas

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,46 @@
 import Header from './components/Header';
 import RestaurantCard from './components/RestaurantCard';
+import { PrismaClient, Cuisine, Location, PRICE } from '@prisma/client';
 
-export default function Home() {
+export interface RestaurantCardType {
+	id: number;
+	name: string;
+	main_image: string;
+	cuisine: Cuisine; // get type from Prisma
+	location: Location;
+	price: PRICE;
+	slug: string;
+}
+
+const prisma = new PrismaClient(); // instance allows to reach out to database directly
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+	const restaurants = await prisma.restaurant.findMany({
+		// selectively pick from db
+		select: {
+			id: true,
+			name: true,
+			main_image: true,
+			cuisine: true,
+			slug: true,
+			location: true,
+			price: true,
+		},
+	});
+
+	return restaurants;
+};
+
+export default async function Home() {
+	const restaurants = await fetchRestaurants();
+
 	return (
 		<main>
 			<Header />
 			<div className='py-3 px-36 mt-10 flex flex-wrap justify-center'>
-				<RestaurantCard />
+				{restaurants.map(restaurant => {
+					return <RestaurantCard restaurant={restaurant} />;
+				})}
 			</div>
 		</main>
 	);
